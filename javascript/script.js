b321679b9346d5150c78d172b8813c4a71a1a775
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-07-18 17:26:52 -0300

    ajustes responsivo

--- javascript/script.js
+++ javascript/script.js
@@ -6,19 +6,32 @@ $(document).ready(function() {
         loop: true,
         nav: true,
         margin: 10
-    })
+    });
        
-})
+});
 
 // Products
 var openProducts = document.querySelector('#products-visible').addEventListener('click', function() {
     document.querySelector('#products').style.visibility="visible"
-})
+});
 
 var closeProducts = document.querySelector('.close').addEventListener('click', function() {
     document.querySelector('#products').style.visibility="hidden"
-})
+});
 
 var openProductsBtn = document.querySelector('#products-visible-btn').addEventListener('click', function() {
     document.querySelector('#products').style.visibility="visible"
-})
+});
+
+// Menu Mobile
+var openProductsMobile = document.querySelector('#products-visible-mobile').addEventListener('click', function() {
+    document.querySelector('#products').style.visibility="visible"
+});
+
+var openMobile = document.querySelector('.btn-mobile').addEventListener('click', function() {
+    document.querySelector('.menu-mobile').style.visibility="visible"
+});
+
+var closeMobile = document.querySelector('.close-menu').addEventListener('click', function() {
+    document.querySelector('.menu-mobile').style.visibility="hidden"
+});
