6c1aa2f5b83996e0640fc293ff07043a6bd0d61b
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-08-31 11:02:03 -0300

    adiciona cart-list

--- javascript/script.js
+++ javascript/script.js
@@ -10,3 +10,11 @@ var openMobile = document.querySelector('.btn-mobile').addEventListener('click',
 var closeMobile = document.querySelector('.close-menu, .menu-mobile').addEventListener('click', function() {
     document.querySelector('.menu-mobile').style.visibility="hidden"
 });
+
+// Btn Adicionar Carrinnho
+const adicionaItem = document.getElementsByClassName('adiciona-produto')
+for(var i = 0; i < adicionaItem.length; i++) {
+    adicionaItem[i].addEventListener('click', function(event) {
+        event.target
+    })
+}
