a72cb3fc041ec7a4720af8fb25a16fff539cad2a
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-09-13 16:30:28 -0300

    altera funcao adiciona ao carrinho

--- javascript/script.js
+++ javascript/script.js
@@ -25,49 +25,9 @@ let cartList = document.querySelector('#cart-listProducts');
 let productName = document.querySelector('#produtoNome');
 let productQntd = document.querySelector('#produtoQntd');
 let productValor = document.querySelector('#produtoValor');
-let btnAdicionaItem = document.querySelectorAll('#adiciona-produto');
+let productImg = document.querySelector('#produtoImg');
+let btnAdicionaItem = document.getElementsByClassName('adiciona-produto');
 let num = 0;
-
-// Lista Objetos
-let allProducts = [
-    {
-        id: 0,
-        name: 'Brigadeiro Belga',
-        qntd: 0,
-        price: [90.00]
-    },
-    {
-        id: 1,
-        name: 'Brigadeiro de Paçoca',
-        qntd: 0,
-        price: [90.00]
-    },
-    {
-        id: 2,
-        name: 'Copinhos de Chocolate',
-        qntd: 0,
-        price: [100.00]
-    },
-    {
-        id: 3,
-        name: 'Brigadeiro de Óreo',
-        qntd: 0,
-        price: [90.00]
-    },
-    {
-        id: 4,
-        name: 'Brigadeiro de Maracujá',
-        qntd: 0,
-        price: [90.00]
-    },
-    {
-        id: 5,
-        name: 'Brigadeiros Gourmet',
-        qntd: 0,
-        price: [100.00]
-    }
-];
-
 
 // Incrementa Valor no Ícone
 for(var i = 0; i < btnAdicionaItem.length; i++) {
@@ -80,16 +40,42 @@ for(var i = 0; i < btnAdicionaItem.length; i++) {
 
 
 // Adiciona produto
-function getProducts() {
-    return allProducts;
-};
-for(var i = 0; i < btnAdicionaItem.length; i++) {
-    btnAdicionaItem[i].addEventListener('click', ()=> {
-        productName.innerHTML = allProducts.name;
-        productQntd.innerHTML = allProducts.qntd;
-        productValor.innerHTML = allProducts.price;
-    });
-};
+let btn1 = document.querySelector('#productSelect-1').addEventListener('click', function() {
+    productName.innerHTML = "Brigaderio Belga";
+    productQntd.innerHTML = "1";
+    productValor.innerHTML = "90";
+    productImg.innerHTML = "<img src='images/brigadeiro.jpg'>"
+});
+let btn2 = document.querySelector('#productSelect-2').addEventListener('click', function() {
+    productName.innerHTML = "Brigaderio de Paçoca";
+    productQntd.innerHTML = "1";
+    productValor.innerHTML = "90";
+    productImg.innerHTML = "<img src='images/pacoca.jpg'>"
+});
+let btn3 = document.querySelector('#productSelect-3').addEventListener('click', function() {
+    productName.innerHTML = "Copinhos de Chocolate";
+    productQntd.innerHTML = "1";
+    productValor.innerHTML = "100";
+    productImg.innerHTML = "<img src='images/copinhos.jpg'>"
+});
+let btn4 = document.querySelector('#productSelect-4').addEventListener('click', function() {
+    productName.innerHTML = "Brigaderio de Óreo";
+    productQntd.innerHTML = "1";
+    productValor.innerHTML = "90";
+    productImg.innerHTML = "<img src='images/bolacha.jpg'>"
+});
+let btn5 = document.querySelector('#productSelect-5').addEventListener('click', function() {
+    productName.innerHTML = "Brigaderio de Maracujá";
+    productQntd.innerHTML = "1";
+    productValor.innerHTML = "90";
+    productImg.innerHTML = "<img src='images/maracuja.jpg'>"
+});
+let btn6 = document.querySelector('#productSelect-6').addEventListener('click', function() {
+    productName.innerHTML = "Brigaderios Gourmett";
+    productQntd.innerHTML = "1";
+    productValor.innerHTML = "100";
+    productImg.innerHTML = "<img src='images/frutas.jpg'>"
+});
 
 // Remove produto
 function ready() {
@@ -101,3 +87,46 @@ function ready() {
         });
     };
 };
+
+
+/* Lista Objetos
+let allProducts = [
+    {
+        id: 0,
+        name: 'Brigadeiro Belga',
+        qntd: 1,
+        price: [90.00]
+    },
+    {
+        id: 1,
+        name: 'Brigadeiro de Paçoca',
+        qntd: 1,
+        price: [90.00]
+    },
+    {
+        id: 2,
+        name: 'Copinhos de Chocolate',
+        qntd: 1,
+        price: [100.00]
+    },
+    {
+        id: 3,
+        name: 'Brigadeiro de Óreo',
+        qntd: 1,
+        price: [90.00]
+    },
+    {
+        id: 4,
+        name: 'Brigadeiro de Maracujá',
+        qntd: 1,
+        price: [90.00]
+    },
+    {
+        id: 5,
+        name: 'Brigadeiros Gourmet',
+        qntd: 1,
+        price: [100.00]
+    }
+];
+
+*/
