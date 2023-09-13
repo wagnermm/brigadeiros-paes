9e64ebdbdde53b885bcf0bef86defadc8457fd7a
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-09-13 10:23:46 -0300

    cria funcao adicionar ao carrinho

--- javascript/script.js
+++ javascript/script.js
@@ -1,31 +1,32 @@
 // Menu Mobile
 var openProductsMobile = document.querySelector('#products-visible-mobile').addEventListener('click', function() {
-    document.querySelector('#products').style.visibility="visible"
+    document.querySelector('#products').style.visibility="visible";
 });
 
 var openMobile = document.querySelector('.btn-mobile').addEventListener('click', function() {
-    document.querySelector('.menu-mobile').style.visibility="visible"
+    document.querySelector('.menu-mobile').style.visibility="visible";
 });
 
 var closeMobile = document.querySelector('.close-menu, .menu-mobile').addEventListener('click', function() {
-    document.querySelector('.menu-mobile').style.visibility="hidden"
+    document.querySelector('.menu-mobile').style.visibility="hidden";
 });
 
 // Shopping Cart
 var listCartIsVisible = document.querySelector('#cart-btn').addEventListener('click', ()=> {
-    document.querySelector('#cart-list').style.visibility='visible'
+    document.querySelector('#cart-list').style.visibility='visible';
 })
 var listCartIsNotVisible = document.querySelector('#close-cartList').addEventListener('click', ()=> {
-    document.querySelector('#cart-list').style.visibility='hidden'
-})
+    document.querySelector('#cart-list').style.visibility='hidden';
+});
 
-// Adicionar Carrinnho
-let cartStatus = document.querySelector('#cart-status')
-let productName = document.querySelector('#produtoNome')
-let productQntd = document.querySelector('#products-number')
-let productValor = document.querySelector('#produtoValor')
-let btnAdicionaItem = document.querySelectorAll('#adiciona-produto')
-
+// Seleciona elementos
+let cartStatus = document.querySelector('#cart-status');
+let cartList = document.querySelector('#cart-listProducts');
+let productName = document.querySelector('#produtoNome');
+let productQntd = document.querySelector('#produtoQntd');
+let productValor = document.querySelector('#produtoValor');
+let btnAdicionaItem = document.querySelectorAll('#adiciona-produto');
+let num = 0;
 
 // Lista Objetos
 let allProducts = [
@@ -65,14 +66,38 @@ let allProducts = [
         qntd: 0,
         price: [100.00]
     }
-]
+];
+
 
 // Incrementa Valor no Ícone
-let num = 0
 for(var i = 0; i < btnAdicionaItem.length; i++) {
     btnAdicionaItem[i].addEventListener('click', ()=> {
         num++
-        num = (num < 6) ? '0' + num : num
-        cartStatus.innerHTML = num
-    })
-}
+        num = (num < 6) ? '0' + num : num;
+        cartStatus.innerHTML = num;
+    });
+};
+
+
+// Adiciona produto
+function getProducts() {
+    return allProducts;
+};
+for(var i = 0; i < btnAdicionaItem.length; i++) {
+    btnAdicionaItem[i].addEventListener('click', ()=> {
+        productName.innerHTML = allProducts.name;
+        productQntd.innerHTML = allProducts.qntd;
+        productValor.innerHTML = allProducts.price;
+    });
+};
+
+// Remove produto
+function ready() {
+    const removeProductButton = document.querySelectorAll('#remove-product');
+    for(var i = 0; i < removeProductButton; i++) {
+        removeProductButton[i].addEventListener('click', function(event) {
+            event.target.parentElement.parentElement.remove();
+            updateTotal()
+        });
+    };
+};
